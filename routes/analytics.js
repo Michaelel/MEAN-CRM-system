b782fad4d3d00b2c3557c7559358d3c110164a8d
michaelel <michaelel1411@gmail.com>
2018-08-21 01:12:30 +0300

    creating routes and controllers

---
+++ routes/analytics.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const controller = require('../controllers/analytics.js')
+const router = express.Router()
+
+router.get('/overview', controller.overview)
+router.get('/analytics', controller.analytics)
+
+
+module.exports = router
